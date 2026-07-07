19bc4ce2460bf25b134bec1bf160d81de62f9eb3
MahdiMohammadiME <mohammadi.mahdi.me@gmail.com>
2026-07-07 09:55:43 +0330

    Restructure the session middleware
    When a request comes in, the middleware shall process the request.
     - Whether it has COOKIES or not
     - If it has COOKIES, check if it has a sessionid cookie
     - Is the session expired or not
     - If the answer of the above questions are `yes`, then extract the
         session data and assign it to the request object.
     - Otherwise, create a new session and assign it to the request object.
    
    In future this process shall be replaced with a function titled
    `process_request`.

--- session/middleware.py
+++ session/middleware.py
@@ -8,12 +8,16 @@ class MySessionMiddleware:
         self.get_response = get_response
 
     def __call__(self, request: HttpRequest) -> HttpResponse:
-        # read cookies
-        # get sessionid from cookies
-        # if sessionid exists, get session data from database
-        # else assign a new MySession obj
-        # request.session = MySession({})
-        # do not forget to check the expiration date
+        if request.COOKIES:
+            request_session_id = request.COOKIES.get("sessionid")
+            # do not forget to check the expiration date
+            if request_session_id and MySession.is_valid(request_session_id):
+                # get session data from database
+                # request.session = MySession._get_session_data(request_session_id)
+                ...
+            else:
+                # request.session = MySession._get_new_session()
+                ...
         response = self.get_response(request)
         # response.set_cookie("sessionid", request.session.session_id)
         # if request.session.is_session_modified:
