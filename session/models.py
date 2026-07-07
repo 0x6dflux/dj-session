ccff76e641f41314485847ecb4642a5a185a743b
MahdiMohammadiME <mohammadi.mahdi.me@gmail.com>
2026-07-07 10:46:27 +0330

    Modify SessionModel
    The expiration_date is a necessary field which shall be defined by the
    SessionManager, not the model. So, the default value for this field is
    removed.

--- session/models.py
+++ session/models.py
@@ -1,16 +1,9 @@
-from datetime import timedelta
-
 from django.db import models
-from django.utils import timezone
-
-
-def get_expiration_datetime():
-    return timezone.now() + timedelta(days=1)
 
 
 class SessionModel(models.Model):
     session_id = models.UUIDField(unique=True)
     session_data = models.JSONField(default=dict)
-    expiration_date = models.DateTimeField(default=get_expiration_datetime)
+    expiration_date = models.DateTimeField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
