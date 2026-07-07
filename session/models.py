c902ee239152cab276287733b5f4f17c6bc61eb1
MahdiMohammadiME <mohammadi.mahdi.me@gmail.com>
2026-07-07 10:43:49 +0330

    Modify SessionModel
    This model shall not contain FK to the user. The user shall be included
    in the session_data. The session itself does not care about the user.

--- session/models.py
+++ session/models.py
@@ -1,6 +1,5 @@
 from datetime import timedelta
 
-from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
@@ -13,11 +12,5 @@ class SessionModel(models.Model):
     session_id = models.UUIDField(unique=True)
     session_data = models.JSONField(default=dict)
     expiration_date = models.DateTimeField(default=get_expiration_datetime)
-    user = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        models.CASCADE,
-        null=True,
-        blank=True,
-    )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
