092fd6cda5a0b2688761614335bd6fa01bf70e80
MahdiMohammadiME <mohammadi.mahdi.me@gmail.com>
2026-07-07 12:46:03 +0330

    Add process request section
    A basic structure to process the incoming request has been added.

--- session/middleware.py
+++ session/middleware.py
@@ -1,6 +1,9 @@
 from typing import Callable
+from uuid import UUID
 
 from django.http import HttpRequest, HttpResponse
+
+from session.session_store import SessionManager
 
 
 class MySessionMiddleware:
@@ -8,16 +11,25 @@ class MySessionMiddleware:
         self.get_response = get_response
 
     def __call__(self, request: HttpRequest) -> HttpResponse:
-        if request.COOKIES:
-            request_session_id = request.COOKIES.get("sessionid")
-            # do not forget to check the expiration date
-            if request_session_id and MySession.is_valid(request_session_id):
-                # get session data from database
-                # request.session = MySession._get_session_data(request_session_id)
-                ...
+        if request_session_id := request.COOKIES.get("sessionid"):
+            print("*** request cookies sessionid", request_session_id)
+            print("*** request cookies sessionid type", type(request_session_id))
+
+            request_session_id = UUID(request_session_id)
+
+            # note that if the request_session_id does not exists in the DB,
+            # the SessionManager will create a new MySession object
+            session_manager = SessionManager.load_or_create_session(request_session_id)
+
+            if session_manager.is_valid():
+                request.session = session_manager.session_obj
             else:
-                # request.session = MySession._get_new_session()
-                ...
+                request.session = SessionManager.create_new_session().session_obj
+
+        else:
+            request.session = SessionManager.create_new_session().session_obj
+        # the above if clause shall refactor later!!
+
         response = self.get_response(request)
         # response.set_cookie("sessionid", request.session.session_id)
         # if request.session.is_session_modified:
